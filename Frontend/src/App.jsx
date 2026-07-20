7af4099ff88a1d92d41a7f4d241b9bc2115e972b
Kalas134 <dk6249@gmail.com>
2026-07-20 11:20:34 +0900

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/AboutPage" element={<AboutPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/Resistry" element={<RegisterPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
