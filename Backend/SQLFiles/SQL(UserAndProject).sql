9a71714961ee4db2cb21b64c91fa66b5a266ade6
Kalas134 <dk6249@gmail.com>
2026-07-20 14:19:53 +0900

    Update SQL(UserAndProject).sql

--- Backend/SQLFiles/SQL(UserAndProject).sql
+++ Backend/SQLFiles/SQL(UserAndProject).sql
@@ -3,16 +3,14 @@ SELECT current_database();
 -- User table
 
 create table Users (
-	USERID varchar(10),
-    USER_ROLE varchar(20), -- User Creator admin
+	USERID varchar(10) PRIMARY KEY,
+    USER_ROLE varchar(20) not null, -- User Creator admin
     PASSWORD varchar(100) not null,
 	NICKNAME varchar(30) unique not null,
 	USERNAME varchar(30) not null,
 	BIRTHDATE DATE not null,
 	PHONE_NUM varchar(20) not null,
 	EMAIL varchar(100) not null,
-    BANK_NAME varchar(30),
-	ACCOUNT_NUM varchar(25),
-	-- 전부 임시임 회의 필요
-	primary key (USER_ROLE, USERID)
+    BANK_NAME varchar(30) not null,
+	ACCOUNT_NUM varchar(25) not null
 );
