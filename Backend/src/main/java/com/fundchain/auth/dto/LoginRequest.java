39e1bf79d20519d27432aadfa5eb52abf2b8d7d6
Kalas134 <dk6249@gmail.com>
2026-07-22 00:08:26 +0900

    오타 및 상세 내용 수정

--- Backend/src/main/java/com/fundchain/auth/dto/LoginRequest.java
+++ Backend/src/main/java/com/fundchain/auth/dto/LoginRequest.java
@@ -1,15 +1,22 @@
 package com.fundchain.auth.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@Table(name = "users")
 @Setter
 @NoArgsConstructor
 public class LoginRequest {
 
+    @Id
+    @Column(name = "USERID", length = 10)
     private String userId;
 
+    @Column(name = "PASSWORD", nullable = false)
     private String password;
 }
--- Backend/src/main/java/com/fundchain/auth/dto/LoginResponse.java
+++ Backend/src/main/java/com/fundchain/auth/dto/LoginResponse.java
@@ -1,17 +1,25 @@
 package com.fundchain.auth.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@Table(name = "users")
 @AllArgsConstructor
 public class LoginResponse {
 
+    @Id
+    @Column(name = "USERID", length = 10)
     private String userId;
 
+    @Column(name = "NICKNAME", nullable = false, unique = true)
     private String nickName;
 
+    @Column(name = "USER_ROLE", nullable = false)
     private String userRole;
 
-    private String token;
+    private String token; // jwt 토큰
 }
