58a7b9d2a2345ee04317be5aa30c1a824986f735
Kalas134 <dk6249@gmail.com>
2026-07-22 05:44:33 +0900

    Update SQL(UserAndProject).sql
    프로젝트 관련 크리에이트 문 추가

--- Backend/SQLFiles/SQL(UserAndProject).sql
+++ Backend/SQLFiles/SQL(UserAndProject).sql
@@ -14,3 +14,42 @@ create table Users (
     BANK_NAME varchar(30) not null,
 	ACCOUNT_NUM varchar(25) not null
 );
+
+create table Projects (
+    PROJECT_ID BIGSERIAL PRIMARY KEY,
+
+    CREATOR_ID varchar(10) not null, --USER_ROLE : Creator에만 해당되는 사람들
+
+    TITLE varchar(100) not null,
+    SHORT_DESC varchar(200) not null,
+
+    TARGET_AMOUNT BIGINT not null,
+
+    START_DATE date not null,
+    END_DATE date not null,
+
+    PROJECT_STATUS varchar(20) not null,
+
+    CREATED_DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT Fk_Projects_Users
+        FOREIGN KEY (CREATOR_ID)
+        REFERENCES Users(USERID)
+);
+
+create table ProjectContent (
+    CONTENT_ID BIGSERIAL PRIMARY KEY,
+
+    PROJECT_ID BIGINT not null,
+
+    CONTENT TEXT not null,
+
+    IMAGE_URL varchar(255),
+
+    CREATED_DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT Fk_ProjectContent_Projects
+        FOREIGN KEY (PROJECT_ID)
+        REFERENCES Projects(PROJECT_ID)
+        ON DELETE CASCADE
+);
