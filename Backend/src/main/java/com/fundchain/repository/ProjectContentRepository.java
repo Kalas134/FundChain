d2091f537c64aa55da79b6125a90550b28c45676
Kalas134 <dk6249@gmail.com>
2026-07-22 23:06:34 +0900

    feat Project repository 파일만 생성
    안에 API가 있지는 않습니다.

---
+++ Backend/src/main/java/com/fundchain/repository/ProjectContentRepository.java
@@ -0,0 +1,8 @@
+package com.fundchain.repository;
+
+import com.fundchain.entity.ProjectContent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectContentRepository extends JpaRepository<ProjectContent, Long> {
+
+}
